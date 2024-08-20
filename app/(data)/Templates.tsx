3cbba7e1c385d8c0030f1d8591718507b00308b3
JC MISA <johncarlomisa399@gmail.com>
2024-08-20 10:00:18 +0800

    add icons for tmeplates

---
+++ app/(data)/Templates.tsx
@@ -0,0 +1,357 @@
+export default [
+  {
+    name: "Blog Title",
+    desc: "An AI tool that generate blog title depends on yout blog information",
+    category: "Blog",
+    icon: "/template-icons/blog-title.png",
+    aiPrompt:
+      "Give me 5 blog topic idea in bullet wise only based on given niche and outline, then give me result in Rich text editor format.",
+    slug: "generate-blog-title",
+    form: [
+      {
+        label: "Enter your blog niche",
+        field: "input",
+        name: "niche",
+        required: true,
+      },
+      {
+        label: "Enter blog outline",
+        field: "textarea",
+        name: "outline",
+      },
+    ],
+  },
+  {
+    name: "Blog Content",
+    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+    category: "blog",
+    icon: "/template-icons/blog-content.png",
+    slug: "blog-content-generation",
+    aiPrompt:
+      "Generate Blog Content based on topic and outline in rich text editor format",
+    form: [
+      {
+        label: "Enter your blog topic",
+        field: "input",
+        name: "topic",
+        required: true,
+      },
+      {
+        label: "Enter blog Outline here",
+        field: "textarea",
+        name: "outline",
+      },
+    ],
+  },
+  {
+    name: "Blog Topic Ideas",
+    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+    category: "Blog",
+    icon: "/template-icons/blog-idea.png",
+    slug: "blog-topic-idea",
+    aiPrompt:
+      "Generate top 5 Blog Topic Ideas in bullet point only, (no Description) based on niche in rich text editor format",
+    form: [
+      {
+        label: "Enter your Niche",
+        field: "input",
+        name: "niche",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Youtube SEO Title",
+    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+    category: "Youtube Tools",
+    icon: "/template-icons/youtube-seo.png",
+    slug: "youtube-seo-title",
+    aiPrompt:
+      "Give me Best SEO optimized high ranked 5 title ideas bullet wise only bases on keywords and outline and give me result in HTML tags format",
+    form: [
+      {
+        label: "Enter your youtube video topic keyowords",
+        field: "input",
+        name: "keywords",
+        required: true,
+      },
+      {
+        label: "Enter youtube description Outline here",
+        field: "textarea",
+        name: "outline",
+      },
+    ],
+  },
+  {
+    name: "Youtube Description",
+    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+    category: "Youtube Tool",
+    icon: "/template-icons/youtube-desc.png",
+    slug: "youtube-description",
+    aiPrompt:
+      "Generate Youtube description with emoji under 4-5 lines based on topic and outline in rich text editor format",
+    form: [
+      {
+        label: "Enter your blog topic/title",
+        field: "input",
+        name: "topic",
+        required: true,
+      },
+      {
+        label: "Enter youtube Outline here",
+        field: "textarea",
+        name: "outline",
+      },
+    ],
+  },
+  {
+    name: "Youtube Tags",
+    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+    category: "Youtube Tool",
+    icon: "/template-icons/youtube-tags.png",
+    slug: "youtube-tag",
+
+    aiPrompt:
+      "Generate 10 Youtube tags in bullet point based on title and outline in rich text editor format",
+
+    form: [
+      {
+        label: "Enter your youtube title",
+        field: "input",
+        name: "title",
+        required: true,
+      },
+      {
+        label: "Enter youtube video Outline here (Optional)",
+        field: "textarea",
+        name: "outline",
+      },
+    ],
+  },
+
+  {
+    name: "Rewrite Article (Plagiarism Free)",
+    desc: "Use this tool to rewrite existing Article or Blog Post which can bypass AI detectors and also make it plagiarism free.",
+    icon: "/template-icons/rewrite.png",
+    category: "Rewriting Tool",
+    slug: "rewrite-article",
+    aiPrompt:
+      "Rewrite the given article without any Plagiarism in rich text editor format",
+    form: [
+      {
+        label:
+          "🤖 Provide your Article/Blogpost or any other content to rewrite.",
+        field: "textarea",
+        name: "article",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Text Improver",
+    desc: "This handy tool refines your writing, eliminating errors and redundancies for a clear, readable result. It also offers a comprehensive tone analysis and suggests better word choices.",
+    icon: "/template-icons/text-improve.png",
+    category: "Writing Assistant",
+    slug: "text-improver",
+    aiPrompt:
+      "Given textToImprove, Rewrite text without any grammar mistake and professionally in rich text editor format",
+    form: [
+      {
+        label: "Enter text that you want to re-write or improve",
+        field: "textarea",
+        name: "textToImprove",
+      },
+    ],
+  },
+  {
+    name: "Add Emojis to Text",
+    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+    icon: "/template-icons/add-emoji.png",
+    category: "blog",
+    slug: "add-emoji-to-text",
+    aiPrompt:
+      "Add Emoji to outline text depends on outline input and rewrite it in rich text editor format",
+    form: [
+      {
+        label: "Enter your text to add emojis",
+        field: "textarea",
+        name: "outline",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Instagram Post Generator",
+    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+    icon: "/template-icons/ig-post.png",
+    category: "blog",
+
+    slug: "instagram-post-generator",
+    aiPrompt:
+      "Generate 3 Instagram post depends on a given keywords and give output in rich text editor format",
+    form: [
+      {
+        label: "Enter Keywords for your post",
+        field: "input",
+        name: "keywords",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Instagram Hash Tag Generator",
+    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+    icon: "/template-icons/ig-hashtag.png",
+    category: "blog",
+
+    slug: "instagram-hash-tag-generator",
+    aiPrompt:
+      "Generate 15 Instagram hash tag depends on a given keywords and give output in rich text editor format",
+    form: [
+      {
+        label: "Enter Keywords for your instagram hastag",
+        field: "input",
+        name: "keywords",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Instagram Post/Reel Idea",
+    desc: "An AI tool that generate New and trending instagram idea depends on your niche",
+    icon: "/template-icons/ig-reel.png",
+    category: "instagram",
+
+    slug: "instagram-post-idea-generator",
+    aiPrompt:
+      "Generate 5-10 Instagram idea depends on niche keywords input with latest trend and give output in  in rich text editor format",
+    form: [
+      {
+        label: "Enter Keywords / Niche for your instagram idea",
+        field: "input",
+        name: "keywords",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "English Grammer Check",
+    desc: "AI Model to Correct your english grammer by providing the text",
+    icon: "/template-icons/grammar.png",
+    category: "english",
+
+    slug: "english-grammer-checker",
+    aiPrompt:
+      "Rewrite the inputText by correcting the grammar and give output in rich text editor format",
+    form: [
+      {
+        label: "Enter text to correct the grammer",
+        field: "input",
+        name: "inputText",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Write Code",
+    desc: "AI Model to generate programming code in any language",
+    icon: "/template-icons/code.png",
+    category: "Coding",
+
+    slug: "write-code",
+    aiPrompt:
+      "Depends on user codeDescription write a code and give output in rich text editor format in code block ",
+    form: [
+      {
+        label: "Enter description of code you want along with Programming Lang",
+        field: "textarea",
+        name: "codeDesscripton",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Explain Code",
+    desc: "AI Model to explain programming code in any language",
+    icon: "/template-icons/explain-code.png",
+    category: "Coding",
+
+    slug: "explain-code",
+    aiPrompt:
+      "Depends on user codeDescription explain code line by line and give output in rich text editor format in code block ",
+    form: [
+      {
+        label: "Enter code which you want to understand",
+        field: "textarea",
+        name: "codeDesscripton",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Code Bug Detector",
+    desc: "This tool analyzes your input, like error messages and code snippets, to pinpoint and fix bugs, offering detailed solutions and alternatives in a straightforward, user-friendly way.",
+    icon: "/template-icons/code-bug.png",
+    category: "code-bug-detector",
+
+    slug: "code-bug-detector",
+    aiPrompt:
+      "Depends on user codeInput find bug in code and give solution and give output in  in rich text editor format in code block ",
+    form: [
+      {
+        label: "Enter code which you want to test bug",
+        field: "textarea",
+        name: "codeInput",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Tagline Generator",
+    desc: "Struggling to find the perfect tagline for your brand? Let our AI-tool assist you in creating a tagline that stands out.",
+    icon: "/template-icons/tag.png",
+    category: "Marketting",
+
+    slug: "tagline-generator",
+    aiPrompt:
+      "Depends on user productName and outline generate catchy 5-10 tagline for the business product and give output in rich text editor format ",
+    form: [
+      {
+        label: "Product/Brand Name",
+        field: "input",
+        name: "productName",
+        required: true,
+      },
+      {
+        label: "What you are selling / Marketting",
+        field: "textarea",
+        name: "outline",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "Product Description",
+    desc: "This is your AI-powered SEO expert, creating captivating and keyword-rich e-commerce product descriptions to boost your online sales.",
+    icon: "/template-icons/product-desc.png",
+    category: "Marketting",
+
+    slug: "product-description",
+    aiPrompt:
+      "Depends on user productName and description generate small description for product for e-commerce business give output in rich text editor format  ",
+    form: [
+      {
+        label: "Product Name",
+        field: "input",
+        name: "productName",
+        required: true,
+      },
+      {
+        label: "Product Details",
+        field: "textarea",
+        name: "outline",
+        required: true,
+      },
+    ],
+  },
+];
